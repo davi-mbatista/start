7c9153f01ba4cc2771eea4f67d2842c3e322a4c8
Daví Machado Batista <machadobatista.davi@gmail.com>
2018-11-15 20:33:51 +0100

    Update template

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 
 import './style.css';
 
-const root = document.querySelector('#root');
-render(<div className="splash">React</div>, root);
+ReactDOM.render(<div className="splash">React</div>, document.querySelector('#root'));
