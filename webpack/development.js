3ba3ff34da842d02821609ad940bdbf2b8ae42dd
Daví Machado Batista <machadobatista.davi@gmail.com>
2019-02-05 23:00:12 +0100

    Refactor build and webpack setup

--- webpack/development.js
+++ webpack/development.js
@@ -1,11 +1,13 @@
 // libs
 const merge = require('webpack-merge');
+const webpack = require('webpack');
 
 // plugins
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 // utils
 const common = require('./common');
+const environment = require('./utils/environment');
 const server = require('./utils/server');
 const paths = require('./utils/paths');
 
@@ -16,12 +18,24 @@ module.exports = merge.smart(common, {
     },
     devServer: server,
     output: {
-        filename: '[name].[chunkhash:8].js',
-        chunkFilename: '[name].chunk.[chunkhash:8].js',
+        filename: '[name].js',
+        chunkFilename: '[name].chunk.js',
         path: paths.APP_BUILD_SRC,
         publicPath: paths.PUBLIC_PATH
     },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                exclude: /(node_modules)/,
+                use: ['style-loader', 'css-loader', 'postcss-loader']
+            }
+        ]
+    },
     plugins: [
+        new webpack.DefinePlugin({
+            'process.env': environment.development
+        }),
         new HtmlWebpackPlugin({
             inject: true,
             template: paths.APP_HTML
