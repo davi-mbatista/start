43c05b01a725cab0437032fb14dd8b342e303c96
Daví Machado Batista <machadobatista.davi@gmail.com>
2019-02-16 23:14:23 +0100

    Update template

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import './style.css';
+
+const splashElement = document.createElement('div');
+
+splashElement.className = 'splash';
+splashElement.innerHTML = 'JS';
+
+document.body.appendChild(splashElement);
