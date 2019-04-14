07b35c1caa9ae7fb0f7f808a025a953e87636768
Daví Machado Batista <machadobatista.davi@gmail.com>
2019-04-14 08:43:51 +0200

    Tweak and comment on development server

--- webpack/utils/server.js
+++ webpack/utils/server.js
@@ -2,17 +2,41 @@
 const paths = require('./paths');
 
 module.exports = {
-    stats: 'minimal',
-    overlay: true,
+    /**
+     * Enables gzip compression for everything that is served.
+     * https://webpack.js.org/configuration/dev-server#devservercompress
+     *  */
+    compress: true,
+    /**
+     * Enables overlay messages only for errors
+     * https://webpack.js.org/configuration/dev-server#devserveroverlay
+     */
+    overlay: {
+        warnings: false,
+        errors: true
+    },
+    /**
+     * Opinionated solution for external access to development server.
+     * Useful for testing application on devices in the same network.
+     * This will initialize the development server with the local IP
+     * but it can be reached from 'localhost' too.
+     *  */
     useLocalIp: true,
     host: '0.0.0.0',
+    /**
+     * "Tell the server where to serve content from. This is only necessary
+     * if you want to serve static files..."
+     * https://webpack.js.org/configuration/dev-server/#devservercontentbase
+     */
     contentBase: paths.APP_PUBLIC,
+    /**
+     * "The bundled files will be available in the browser under this path..."
+     * https://webpack.js.org/configuration/dev-server/#devserverpublicpath
+     */
     publicPath: paths.PUBLIC_PATH,
-    watchContentBase: true,
-    watchOptions: {
-        ignored: paths.NODE_MODULES
-    },
-    historyApiFallback: {
-        disableDotRule: true
-    }
+    /**
+     * The development server will watch the files served by the contentBase
+     * https://webpack.js.org/configuration/dev-server/#devserverwatchcontentbase
+     */
+    watchContentBase: true
 };
