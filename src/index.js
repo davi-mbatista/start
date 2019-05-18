fa2bde22e3493540e6e081dfe238a729de6dac1d
Daví Machado Batista <machadobatista.davi@gmail.com>
2019-05-18 17:35:12 +0200

    Update template
    Signed-off-by: Daví Machado Batista <machadobatista.davi@gmail.com>

--- src/index.js
+++ src/index.js
@@ -3,7 +3,10 @@ import { html, render } from 'lit-html';
 import './style.css';
 
 const hello = () => html`
-    <div class="splash">Start here - lit-html</div>
+    <div class="splash">
+        <div class="lit-logo">lit-html</div>
+        <div>Open <mark>src/index.js</mark> to get started.</div>
+    </div>
 `;
 
 render(hello(), document.body);
