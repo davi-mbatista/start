688262f18d62365eed1a120986bf948f07155f51
Daví Machado Batista <machadobatista.davi@gmail.com>
2019-05-18 15:59:20 +0200

    Update loaders and add svelte specific resolvers

--- webpack/common.js
+++ webpack/common.js
@@ -8,10 +8,25 @@ module.exports = {
                 loader: 'eslint-loader'
             },
             {
+                test: /\.svelte$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'svelte-loader',
+                    options: {
+                        emitCss: true,
+                        hotReload: true
+                    }
+                }
+            },
+            {
                 test: /\.js$/,
                 exclude: /(node_modules)/,
                 loader: 'babel-loader'
             }
         ]
+    },
+
+    resolve: {
+        extensions: ['.mjs', '.js', '.svelte']
     }
 };
