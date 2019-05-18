c6fc70baad64d0a53f855124154c79fdf05a0d09
Daví Machado Batista <machadobatista.davi@gmail.com>
2019-05-18 16:45:16 +0200

    Update template
    Signed-off-by: Daví Machado Batista <machadobatista.davi@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import './style.css';
+import JSLogo from './JSLogo.svg';
 
 const splashElement = document.createElement('div');
 
 splashElement.className = 'splash';
-splashElement.innerHTML = 'Start here';
+splashElement.innerHTML = `<img src="${JSLogo}" alt="JavaScript logo"/> <div> Open <mark>src/index.js</mark> to get started.</div>`;
 
 document.body.appendChild(splashElement);
